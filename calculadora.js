f74753234221b6bb5eca40a31cc7ede92b3b656e
Emesson Silva <140443316+emessonSilva@users.noreply.github.com>
2023-09-30 00:12:59 -0300

    Update calculadora.js

--- calculadora.js
+++ calculadora.js
@@ -1,26 +1,27 @@
-//Calculadora simples em JS
+//Calculadora bem simples em JS
 
 const valor1 = Number(prompt("Digite um número:"));
 const valor2 = Number(prompt("Digite um número:"));
 let operador = prompt("Escolha qual operação deseja realizar: + (soma), - (subtração), / (divisão) ou x (multiplicação)");
 
 let resultado;
+let resto; 
 
 switch (operador) {
     case '+':
         resultado = valor1 + valor2;
         break;
-    
-    case '-': 
+
+    case '-':
         resultado = valor1 - valor2;
         break;
-    
-    case '/': 
-       if (valor2 !== 0) {
-        resultado = valor1 / valor2;
-        resto = valor1 % valor2
+
+    case '/':
+        if (valor2 !== 0) {
+            resultado = valor1 / valor2;
+            resto = valor1 % valor2;
         } else {
-        alert("Erro: Divisão por zero.");
+            alert("Erro: Divisão por zero.");
         }
         break;
 
@@ -29,17 +30,17 @@ switch (operador) {
         break;
 
     default:
-        console.log("Operador inválido.");
+        alert("Operador inválido.");
         break;
 }
 
 if (resultado !== undefined) {
     if (operador === '/') {
-        alert("O resultado da divisão é: " + resultado);
+        alert(`O resultado da divisão é: ${resultado}`);
         if (resto !== undefined) {
-            alert("A sobra da divisão é: " + resto);
+            alert(`A sobra da divisão é: ${resto}`);
         }
     } else {
-        alert("O resultado da operação " + operador + " é: " + resultado);
+        alert(`O resultado da operação é: ${resultado}`);
     }
 }
